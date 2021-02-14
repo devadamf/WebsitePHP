e9ea6d5c4e16b82a20cfca33cf90078721b47737
Adam Fahrenkamp <49660485+devadamf@users.noreply.github.com>
2021-02-14 11:28:13 -0600

    added Zines to German presse

--- de/presse.php
+++ de/presse.php
@@ -132,21 +132,25 @@
                 </li>
                 <li>„Von strafrechtsfeministische zu abolitionistische Sicherheit für Betroffene gegenderter
                     Gewalt“ in <a href='https://www.dampfboot-verlag.de/shop/artikel/sicherheit'
-                            target="_blank"><em>Abolishing the Police</em></a>, edited by Koshka Duff (Dog Section Press)
+                                  target="_blank"><em>Abolishing the Police</em></a>, edited by Koshka Duff (Dog Section
+                    Press)
                 </li>
             </ul>
 
-
-            <ul>
-                <li><a href='https://www.transformativejustice.eu/wp-content/uploads/2017/04/Das-Risiko-wagen.pdf'
-                       target="_blank">„Das Risiko wagen. Strategien für selbstorganisierte & kollektive
-                        Verantwortungsübernahme bei sexualisierter Gewalt“</a> Übersetzung eines Textes von Community
-                    Against Rape and Abuse mit Einleitung vom Transformative Justice Kollektiv Berlin. <a
-                            href='https://cinselsiddetlemucadele.org/wp-content/uploads/2019/12/devrim-evde-baslar.pdf'
-                            target="_blank">Übersetzung auf Türkisch</a> von Özge Karlık, Cinsel Şiddetle Mücadele
-                    (Verband für den Kampf gegen sexualisierter Gewalt).
-                </li>
-            </ul>
+            <section>
+                <h2><span class='h2-underline'>Zines</span></h2>
+                <ul>
+                    <li><a href='https://www.transformativejustice.eu/wp-content/uploads/2017/04/Das-Risiko-wagen.pdf'
+                           target="_blank">„Das Risiko wagen. Strategien für selbstorganisierte & kollektive
+                            Verantwortungsübernahme bei sexualisierter Gewalt“</a> Übersetzung eines Textes von
+                        Community
+                        Against Rape and Abuse mit Einleitung vom Transformative Justice Kollektiv Berlin. <a
+                                href='https://cinselsiddetlemucadele.org/wp-content/uploads/2019/12/devrim-evde-baslar.pdf'
+                                target="_blank">Übersetzung auf Türkisch</a> von Özge Karlık, Cinsel Şiddetle Mücadele
+                        (Verband für den Kampf gegen sexualisierter Gewalt).
+                    </li>
+                </ul>
+            </section>
         </section>
 
         <?php
