909a0e05b532bf2ff39701cc01aad44afe809ff3
Adam Fahrenkamp <49660485+devadamf@users.noreply.github.com>
2021-02-14 13:08:04 -0600

    roundtable corrections

--- de/forschungsroundtable.php
+++ de/forschungsroundtable.php
@@ -1,45 +1,58 @@
 <?php include('header.php'); ?>
-    <div id='content'>
-        <?php main_nav("forschungsroundtable", TRUE); ?>
-        <?php main_nav("forschungsroundtable", FALSE); ?>
+<div id='content'>
+    <?php main_nav("forschungsroundtable", TRUE); ?>
+    <?php main_nav("forschungsroundtable", FALSE); ?>
 
-        <div class='content-main'>
-            <section>
-                <h2><span class='h2-underline'>Forschungs Roundtable</h2>
-                <p class='research-text'>
-                    Interessierst du dich für Forschung zu transformativer Gerechtigkeit?
-                </p>
-                <p class='research-text' style="margin-top: 30px">
-                    Arbeitest du zu den Themen:
-                </p>
-                <br />
-                <ul class='research-list'>
-                    <li>sexualisierte Gewalt & Beziehungsgewalt</li>
-                    <li>Rassismus & staatliche Gewalt (Überwachung, Grenzregime, Gefängnisse)</li>
-                    <li>soziale Bewegungen gegen Gewalt</li>
-                    <li>Sexarbeit</li>
-                    <li>Strafrechtsfeminismus/Femonationalismus/Instrumentalisierung von Queerfeminismus durch neoliberale & rechte Kräfte</li>
-                    <li>Kritik an Bestrafung & Kriminologie</li>
-                    <li>Disability Justice & Kritik an Psychiatrie</li>
-                    <li>restorativer und transformativer Gerechtigkeit</li>
-                    <li>Konfliktvermittlung</li>
-                </ul>
-                <p class='research-text'>
-                    Interessierst du dich dafür, diesen Themen durch eine transformative, abolitionistische Perspektive zu forschen?
-                </p>
-                <p class='research-text'>
-                    Das „Was macht uns wirklich sicher?“ Projekt lädt jährlich die Menschen zu einem runden Tisch ein, welche zu diesen Themen wissenschaftlich arbeiten, oder dies vorhaben. Dabei verstehen wir Forschung nicht nur in einem akademischen oder professionellen Sinne, sondern als ein konzentriertes Lernen. Unser Ziel ist eine transnationale Netzwerk von Forscher*innen (aktuell aus Australien, Kanada, UK, Deutschland, und U.S.) zu entwickeln. Unser erster runder Tisch fand im August 2018 in Berlin statt und wir planen weitere Veranstaltungen in San Francisco bei der National Women’s Studies Association Jahreskonferenz in November 2019, und in London in 2020.
-                </p>
-                <p class='research-text'>
-                    Beim runden Tisch, teilen wir unsere Forschungsprojekte mit einander (ganz in Ordnung wenn Du noch keins hast!), bearbeiten unsere gemeinsamen Fragen und entwickeln ein Rahmenplan für das wie und warum von abolitionistischer Forschung. Wenn du Interesse an der Teilnahme hast, schreib bitte eine Mail mit einer Beschreibung deiner aktuellen oder künftigen Forschungsinteressen und was du aus dem Roundtable mitnehmen möchtest, an whatreallymakesussafe@riseup.net.
-                </p>
-            </section>
-        </div>
-
+    <div class='content-main'>
+        <section>
+            <h2><span class='h2-underline'>Forschungs Roundtable</h2>
+            <p class='research-text'>
+                Interessierst du dich für Forschung zu transformativer Gerechtigkeit?
+            </p>
+            <p class='research-text' style="margin-top: 30px">
+                Arbeitest du zu den Themen:
+            </p>
+            <br/>
+            <ul class='research-list'>
+                <li>sexualisierte Gewalt & Beziehungsgewalt</li>
+                <li>Rassismus & staatliche Gewalt (Überwachung, Grenzregime, Gefängnisse)</li>
+                <li>soziale Bewegungen gegen Gewalt</li>
+                <li>Sexarbeit</li>
+                <li>Strafrechtsfeminismus/Femonationalismus/Instrumentalisierung von Queerfeminismus durch neoliberale &
+                    rechte Kräfte
+                </li>
+                <li>Kritik an Bestrafung & Kriminologie</li>
+                <li>Disability Justice & Kritik an Psychiatrie</li>
+                <li>restorativer und transformativer Gerechtigkeit</li>
+                <li>Konfliktvermittlung</li>
+            </ul>
+            <p class='research-text'>
+                Interessierst du dich dafür, diesen Themen durch eine transformative, abolitionistische Perspektive zu
+                forschen?
+            </p>
+            <p class='research-text'>
+                Das „Was macht uns wirklich sicher?“ Projekt lädt jährlich die Menschen zu einem runden Tisch ein,
+                welche zu diesen Themen wissenschaftlich arbeiten, oder dies vorhaben. Dabei verstehen wir Forschung
+                nicht nur in einem akademischen oder professionellen Sinne, sondern als ein konzentriertes Lernen. Unser
+                Ziel ist eine transnationale Netzwerk von Forscher*innen (aktuell aus Australien, Kanada, UK,
+                Deutschland, und U.S.) zu entwickeln. Für Beispiele von unserer Arbeit, siehe das <a
+                        href="http://whatreallymakesussafe.com/#/trans" target="_blank">„Transnational“</a>
+            Bereich von dieser Webseite.
+            </p>
+            <p class='research-text'>
+                Beim runden Tisch, teilen wir unsere Forschungsprojekte mit einander (ganz in Ordnung wenn Du noch keins
+                hast!), bearbeiten unsere gemeinsamen Fragen und entwickeln ein Rahmenplan für das wie und warum von
+                abolitionistischer Forschung. Wenn du Interesse an der Teilnahme hast, schreib bitte eine Mail mit einer
+                Beschreibung deiner aktuellen oder künftigen Forschungsinteressen und was du aus dem Roundtable
+                mitnehmen möchtest, an whatreallymakesussafe@riseup.net.
+            </p>
+        </section>
     </div>
-    <footer style='text-align: center'>
-        <p>&copy; 2021 WRMUS</p>
-    </footer>
+
+</div>
+<footer style='text-align: center'>
+    <p>&copy; 2021 WRMUS</p>
+</footer>
 
 </body>
 
